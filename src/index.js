88a2ef60b3c8e668f24b7a099920c9345786aefe
Jason Whitted <jwhitted@ent.com>
2017-06-29 13:43:34 -0600

    Add changePath as an export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as _actions from './actions';
 export const actions = _actions;
-export const { initialize, remove, set } = _actions;
+export const { changePath, initialize, remove, set } = _actions;
 export * from './actionTypes';
 export * from './selectors';
 export { default as initialState } from './initialState';
